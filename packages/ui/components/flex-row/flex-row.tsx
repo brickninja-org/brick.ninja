64dba0ad01bcdf9fe99e421196016fb8d4d41d57
brickninja-pro <dev@brick.ninja>
2024-07-28 22:59:39 +0200

    Add FlexRow component

---
+++ packages/ui/components/flex-row/flex-row.tsx
@@ -0,0 +1,38 @@
+import type {FC, ReactNode} from "react";
+
+import {tv, type VariantProps} from "tailwind-variants";
+
+import { cn } from "../../lib/tailwind";
+
+const flex = tv({
+  base: "flex gap-2 mt-4 items-center",
+  variants: {
+    align: {
+      left: "justify-start",
+      right: "justify-end",
+      center: "justify-center",
+      between: "justify-between",
+    },
+    wrap: {
+      false: "flex-nowrap",
+      true: "flex-wrap",
+    },
+  },
+  defaultVariants: {
+    align: "left",
+    wrap: false,
+  },
+});
+
+export type FlexVariants = VariantProps<typeof flex>;
+
+interface FlexRowProps extends FlexVariants {
+  className?: string;
+  children: ReactNode;
+}
+
+export const FlexRow: FC<FlexRowProps> = (props) => {
+  const {children, className, align, wrap} = props;
+
+  return <div className={cn(flex({align, wrap}), className)}>{children}</div>;
+};
