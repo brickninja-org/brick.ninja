964ba37ac5a623911ec6753ccb9e8ccf9fbea6bb
brickninja-pro <dev@brick.ninja>
2024-09-15 12:36:14 +0200

    chore(web): add status API page

---
+++ apps/web/app/[language]/status/api/page.tsx
@@ -0,0 +1,103 @@
+import { Fragment } from 'react';
+
+import type { ApiRequest } from '@brickninja-org/database';
+import { Headline } from '@brickninja-org/ui/components/headline';
+import { Table } from '@brickninja-org/ui/components/table';
+
+import { db } from '@/lib/prisma';
+import { FormatNumber } from '@/components/format/format-number';
+import { PageLayout } from '@/components/layout/page-layout';
+
+import { availablePeriods } from './available-periods';
+import { cn } from '@brickninja-org/ui/lib';
+
+async function getData(hours: number) {
+  const now = new Date();
+
+  const past = new Date();
+  past.setHours(now.getHours() - hours);
+
+  const apiRequests = await db.apiRequest.findMany({ where: { createdAt: { gte: past }}, orderBy: { createdAt: 'desc' }});
+
+  const endpoints: Record<string, { totalResponseTimeMs: number, requestCount: number, errors: number, lastRequests: boolean[], requests: ApiRequest[] }> = {};
+  const statusCodes: Record<number, number> = {};
+  let errors = 0;
+
+  apiRequests.forEach((request) => {
+    if (!endpoints[request.endpoint]) {
+      endpoints[request.endpoint] = { totalResponseTimeMs: 0, requestCount: 0, errors: 0, lastRequests: [], requests: [] };
+    }
+
+    endpoints[request.endpoint].requests.push(request);
+    endpoints[request.endpoint].totalResponseTimeMs += request.responseTimeMs;
+    endpoints[request.endpoint].requestCount++;
+    statusCodes[request.status] = (statusCodes[request.status] ?? 0) + 1;
+
+    if (request.status !== 200) {
+      errors++;
+      endpoints[request.endpoint].errors++;
+    }
+
+    if (endpoints[request.endpoint].lastRequests.length < 100) {
+      endpoints[request.endpoint].lastRequests.push(request.status === 200);
+    }
+  });
+
+  return { total: apiRequests.length, errors, endpoints, statusCodes, apiRequests };
+}
+
+export default async function StatusApiPage({ searchParams: { period }}: { searchParams: { period?: string }}) {
+  const hours = availablePeriods.find(({ value }) => value == period)?.hours ?? 24;
+
+  const { endpoints, errors, total, statusCodes } = await getData(hours);
+  return (
+    <PageLayout>
+      <Headline id="api-status">Brickset API status</Headline>
+
+      <p>
+        <FormatNumber value={total}/> requests and <FormatNumber value={errors}/> errors (<FormatNumber value={errors / total * 100} unit="%"/>) in the last {hours} hours.
+      </p>
+
+      <Headline id="status-codes">Status Codes ({hours}h)</Headline>
+      <div className="grid [grid-template-columns:_auto_1fr]">
+        {Object.entries(statusCodes).map(([statusCode, amount]) => (
+          <Fragment key={statusCode}>
+            <div className="p-2 border-r-2 font-medium">{statusCode}</div>
+            <div className="relative flex items-center p-2">
+              <div style={{ width: `${amount / total * 100}%` }} className={cn(['h-4 rounded-sm', statusCode === '200' ? 'bg-green-600' : 'bg-red-600'])}/>
+              <div className="ml-2">{amount}</div>
+            </div>
+          </Fragment>
+        ))}
+      </div>
+
+      <Headline id="endpoints">Endpoints ({hours}h)</Headline>
+      <Table>
+        <thead>
+          <tr>
+            <th>Endpoint</th>
+            <th align="right">Avg. Response Time</th>
+            <th align="right">Requests</th>
+            <th>Errors</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(endpoints).sort(([a], [b]) => a.localeCompare(b)).map(([ endpoint, data ]) => (
+            <tr key={endpoint}>
+              <th>{endpoint}</th>
+              <td align="right">
+                <FormatNumber value={Math.round(data.totalResponseTimeMs / data.requestCount)} unit="ms"/>
+              </td>
+              <td align="right">
+                <FormatNumber value={data.requestCount}/>
+              </td>
+              <td>
+                <FormatNumber value={data.errors}/> (<FormatNumber value={data.errors / data.requestCount * 100} unit="%"/>)
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </PageLayout>
+  );
+}
