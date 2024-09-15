47515012ee56ba45b2f18b8f36812275438ce7b2
brickninja-pro <dev@brick.ninja>
2024-09-15 17:21:49 +0200

    fix(web): fix typescript error link-properties

--- apps/web/lib/link-properties.ts
+++ apps/web/lib/link-properties.ts
@@ -3,6 +3,7 @@ import type { LocalizedEntity } from './localized-name';
 export const linkPropertiesBase = { id: true, name_en: true, name_nl: true } as const;
 export const linkProperties = { ...linkPropertiesBase } as const;
 
+export function getLinkProperties<T extends LocalizedEntity & { id: unknown }>(value: T): LocalizedEntity & { id: T['id'] };
 export function getLinkProperties(
   { id, name_en, name_nl }: LocalizedEntity & { id: unknown },
 ) {
