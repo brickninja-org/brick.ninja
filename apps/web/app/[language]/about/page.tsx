e782e514015a04afcaed07213de9a80589afd535
brickninja-pro <dev@brick.ninja>
2024-09-22 02:52:16 +0200

    chore(web): add about page

---
+++ apps/web/app/[language]/about/page.tsx
@@ -0,0 +1,20 @@
+import { Headline } from '@brickninja-org/ui/components/headline';
+
+import { HeroLayout } from '@/components/layout/hero-layout';
+import { TableOfContentAnchor } from '@brickninja-org/ui/components/table-of-content';
+import type { Metadata } from 'next';
+
+export default function AboutPage() {
+  return (
+    <HeroLayout hero={<Headline id="about-page">About</Headline>} toc>
+      <TableOfContentAnchor id="about">About</TableOfContentAnchor>
+      <p><strong>brick.ninja</strong> is a database website for LEGO&reg; sets, minifigures and parts. Development started in July 2024. It is completly written using modern technology.</p>
+
+      <Headline id="technology">Technology</Headline>
+    </HeroLayout>
+  );
+}
+
+export const metadata: Metadata = {
+  title: 'About',
+};
