4b707275eaf2a2d771f1fa677d07816c5529cfdb
brickninja-pro <dev@brick.ninja>
2024-09-26 23:47:00 +0200

    fix(ui): add CSS properties types

--- packages/ui/types.d.ts
+++ packages/ui/types.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-dom/canary" />
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type * as CSS from 'csstype';
+
+declare module 'csstype' {
+  interface Properties {
+    // add known custom properties
+    '--icon-size'?: `${string}px`;
+    '--icon-color'?: CSS.Property.Color,
+
+    // allow all custom properties
+    [index: `--${string}`]: string | number;
+  }
+}
