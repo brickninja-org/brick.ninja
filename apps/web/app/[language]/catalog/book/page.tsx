326dd8051181616c988404b6975d3f352c63ebb6
brickninja-pro <dev@brick.ninja>
2024-09-27 19:24:26 +0200

    fix(web): update catalog books page

--- apps/web/app/[language]/catalog/book/page.tsx
+++ apps/web/app/[language]/catalog/book/page.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from 'next';
+import { unstable_cache } from 'next/cache';
 
+import type { Language } from '@brickninja-org/database';
 import { createDataTable } from '@brickninja-org/ui/components/table/data-table';
 
 import type { PageProps } from '@/lib/next';
 import { db } from '@/lib/prisma';
+import { localizedName } from '@/lib/localized-name';
 import { translate } from '@/lib/translate';
 import { getAlternateUrls } from '@/lib/url';
+import { FormatNumber } from '@/components/format/format-number';
 import { Translate } from '@/components/i18n/translate';
 import { Description } from '@/components/layout/description';
 import { ColumnSelect } from '@/components/table/column-select';
-import { unstable_cache } from 'next/cache';
-import type { Language } from '@brickninja-org/database';
-import { localizedName } from '@/lib/localized-name';
 
 const getItems = unstable_cache((language: Language) => {
   return db.item.findMany({
@@ -21,6 +22,8 @@ const getItems = unstable_cache((language: Language) => {
       name_en: language === 'en',
       name_nl: language === 'nl',
       productCode: true,
+      minifigureCount: true,
+      pieceCount: true,
     },
     orderBy: { productCode: 'asc' },
   });
@@ -40,9 +43,11 @@ export default async function CatalogBookPage({ params }: PageProps) {
       </Description>
 
       <Books.Table>
-        <Books.Column id="id" title="ID" align="right" small hidden>{({ id }) => id}</Books.Column> 
-        <Books.Column id="productCode" title="Product Code" align="right" sortBy="productCode">{({ productCode }) => productCode}</Books.Column>
-        <Books.Column id="name" title="Name" align="left">{(item) => localizedName(item, params.language) }</Books.Column>
+        <Books.Column id="id" title={<Translate id="itemTable.column.id"/>} align="right" small hidden>{({ id }) => id}</Books.Column> 
+        <Books.Column id="productCode" title={<Translate id="itemTable.column.productCode"/>} sortBy="productCode">{({ productCode }) => productCode}</Books.Column>
+        <Books.Column id="name" title={<Translate id="itemTable.column.item"/>} align="left">{(item) => localizedName(item, params.language) }</Books.Column>
+        <Books.Column id="minifigureCount" title={<Translate id="itemTable.column.minifigureCount"/>} align="right" sortBy="minifigureCount">{({ minifigureCount }) => <FormatNumber value={minifigureCount}/>}</Books.Column>
+        <Books.Column id="pieceCount" title={<Translate id="itemTable.column.pieceCount"/>} align="right" sortBy="pieceCount">{({ pieceCount }) => <FormatNumber value={pieceCount}/>}</Books.Column>
       </Books.Table>
     </>
   );
