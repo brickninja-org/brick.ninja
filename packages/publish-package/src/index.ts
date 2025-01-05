804ce2b124c10b110efba3d7b17c4bb59f9c99f6
brickninja-pro <dev@brick.ninja>
2025-01-05 20:34:33 +0100

    fix(docker): update docker files

--- packages/publish-package/src/index.ts
+++ packages/publish-package/src/index.ts
@@ -1,11 +1,21 @@
-#!/usr/bin/env node
 import fs from 'node:fs';
 import { execSync } from 'node:child_process';
+import path from 'node:path';
 
 async function run() {
+  const packageJsonPath = path.join(process.cwd(), 'package.json');
+
   // get local package version
-  const packageName = process.env.npm_package_name;
-  const packageVersion = process.env.npm_package_version;
+  if(!fs.existsSync(packageJsonPath)) {
+    console.error('Could not find package.json');
+    process.exit(1);
+  }
+
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+  const packageName = packageJson.name;
+  const packageVersion = packageJson.version;
+  const packageTag = packageJson.publishConfig?.tag ?? 'latest';
 
   if(!packageName || !packageVersion) {
     console.error('Could not load local package data, make sure to run this script from within a package.json and using npm.');
@@ -16,8 +26,7 @@ async function run() {
   const githubOutput = process.env.GITHUB_OUTPUT;
 
   if(!githubEventName) {
-    console.error('Could not read github.event_name');
-    process.exit(1);
+    console.warn('Could not read github.event_name, this is a dry-run');
   }
 
   if(!githubOutput) {
@@ -25,30 +34,38 @@ async function run() {
   }
 
   // get latest remote version
-  const remote: 'Not Found' | { version: string } = await fetch(`https://registry.npmjs.org/${packageName}/latest`).then((r) => r.json());
+  const remote: 'Not Found' | { version: string } = await fetch(`https://registry.npmjs.org/${packageName}/${packageTag}`).then((r) => r.json());
+
+  console.log(`Package: ${packageName}`);
+  console.log(`Local version: ${packageVersion}`);
+  console.log(`Remote version: ${remote === 'Not Found' ? '-' : remote.version}`);
+  console.log();
 
   if(remote === 'Not Found') {
-    console.log(`::notice::Publishing new package ${packageName}@${packageVersion}`);
+    console.log(`::notice::Publishing new package ${packageName}@${packageVersion} as ${packageTag}`);
   } else {
     if(remote.version === packageVersion) {
       console.log('Nothing to publish');
       return;
     }
 
-    console.log(`::notice::Publishing package ${packageName}@${packageVersion}`);
+    console.log(`::notice::Publishing package ${packageName}@${packageVersion} as ${packageTag}`);
   }
 
   if(githubOutput) {
-    fs.appendFileSync(githubOutput, `publish=${packageName}@${packageVersion}`);
+    fs.appendFileSync(githubOutput, `publish=${packageName}@${packageVersion}\n`);
+    fs.appendFileSync(githubOutput, `package=${packageName}\n`);
+    fs.appendFileSync(githubOutput, `version=${packageVersion}\n`);
+    fs.appendFileSync(githubOutput, `tag=${packageTag}\n`);
   }
 
   if(githubEventName !== 'push') {
-    execSync('npm publish --access=restricted --dry-run');
+    execSync(`pnpm publish --dry-run --no-git-checks --tag ${packageTag}`, { stdio: 'inherit' });
 
     return;
   }
 
-  execSync('npm publish --access=restricted');
+  execSync(`pnpm publish --tag ${packageTag}`, { stdio: 'inherit' });
 }
 
 run();
