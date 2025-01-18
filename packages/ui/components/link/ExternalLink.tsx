906b4e8bb6a5f3eb599286aa411c4afbb6d6b560
brickninja-pro <dev@brick.ninja>
2025-01-18 01:30:20 +0100

    chore(ui): add ExternalLink

---
+++ packages/ui/components/link/ExternalLink.tsx
@@ -0,0 +1,19 @@
+import type { FC, ReactNode } from 'react';
+import { Icon } from '../../icons';
+
+export interface ExternalLinkProps {
+  href: string;
+  target?: string;
+  children: ReactNode;
+}
+
+export const ExternalLink: FC<ExternalLinkProps> = ({ href, target, children }) => {
+  // the `&#8203;` in combination with `white-space: nowrap` on `.icon` prevents wrapping
+  // between the link content and the icon, but still allows wrapping within the content.
+  return (
+    <a href={href} target={target} rel="noopener noreferrer" className="group focus-visible:outline-(--color-focus) hover:decoration-transparent">
+      <span className="group-hover:decoration-current group-hover:transition-colors group-hover:duration-150 underline underline-offset-2 decoration-2 decoration-transparent transition-colors duration-300 whitespace-normal">{children}</span>
+      <span className="whitespace-nowrap">&#8203;<Icon icon="arrow-up-right"/></span>
+    </a>
+  );
+};
