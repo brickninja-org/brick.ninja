305e20588f2689cb6625c5403be9e8e4249e9ac3
brickninja-pro <dev@brick.ninja>
2025-03-23 21:56:37 +0100

    fix(web): change cookie notice

--- apps/web/components/item-table/ItemTableColumnsButton.client.tsx
+++ apps/web/components/item-table/ItemTableColumnsButton.client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import type { FC } from 'react';
+import { Suspense, type FC } from 'react';
 import type { TranslationSubset } from '@/lib/translate';
 
 import { Dropdown } from '@brickninja-org/ui/components/dropdown/Dropdown';
@@ -8,6 +8,7 @@ import { Button } from '@brickninja-org/ui/components/form/Button';
 import { Checkbox } from '@brickninja-org/ui/components/form/Checkbox';
 import { MenuList } from '@brickninja-org/ui/components/layout/MenuList';
 import { Separator } from '@brickninja-org/ui/components/layout/Separator';
+import { Skeleton } from '@/components/skeleton/Skeleton';
 import { Icon } from '@brickninja-org/ui/icons';
 
 import { useUser } from '@/components/user/use-user';
@@ -19,7 +20,6 @@ export interface ItemTableColumnsButtonProps {
 }
 
 export const ItemTableColumnsButton: FC<ItemTableColumnsButtonProps> = ({ translations }) => {
-  const { user } = useUser();
   const { availableColumns, selectedColumns, defaultColumns, setSelectedColumns } = useItemTableContext();
   const columns = selectedColumns ?? defaultColumns;
 
@@ -28,12 +28,9 @@ export const ItemTableColumnsButton: FC<ItemTableColumnsButtonProps> = ({ transl
   return (
     <Dropdown button={<Button icon={<Icon icon="table-insert-column"/>}>{translations['table.columns']}</Button>} preferredPlacement="right-start">
       <MenuList>
-        {!user && (
-          <div className="flex items-center gap-3 mb-2 py-1 px-4 max-w-[200px] rounded-xs border bg-gray-100">
-            <Icon icon="cookie"/>
-            Changing columns will store cookies in your browser
-          </div>
-        )}
+        <Suspense fallback={<Skeleton/>}>
+          <ItemTableColumnsButtonCookieNotice/>
+        </Suspense>
         {values.map((column) => (
           <Checkbox key={column.id} checked={columns.includes(column.id)} onChange={(checked) => setSelectedColumns(values.filter(({ id }) => id !== column.id ? columns.includes(id) : checked).map(({ id }) => id))}>{column.title}</Checkbox>
         ))}
@@ -41,5 +38,16 @@ export const ItemTableColumnsButton: FC<ItemTableColumnsButtonProps> = ({ transl
         <Button appearance="menu" onClick={() => setSelectedColumns(undefined)} disabled={selectedColumns === undefined}>{translations['table.columns.reset']}</Button>
       </MenuList>
     </Dropdown>
+  );
+};
+
+export const ItemTableColumnsButtonCookieNotice = () => {
+  const user = useUser();
+
+  return !user && (
+    <div className="flex gap-3 items-center py-1 px-4 max-w-[264px] min-w-full mb-2 bg-gray-100 rounded-xs border border-gray-300 leading-normal">
+      <Icon icon="cookie"/>
+      Changing columns will store cookies in your browser.
+    </div>
   );
 };
