42b757f7f6227dbc8fc510e095d01170a4c8ec69
brickninja-pro <dev@brick.ninja>
2025-03-23 21:34:46 +0100

    fix(web): update Profile page

--- apps/web/app/[language]/profile/page.tsx
+++ apps/web/app/[language]/profile/page.tsx
@@ -8,18 +8,22 @@ import { Table } from '@brickninja-org/ui/components/table/Table';
 import { getUser } from '@/lib/get-user';
 import { db } from '@/lib/prisma';
 import { HeroLayout } from '@/components/layout/HeroLayout';
+import { pageView } from '@/lib/page-view';
+import { FormatDate } from '@/components/format/FormatDate';
+import type { Metadata } from 'next';
+import { revalidatePath } from 'next/cache';
+import { SubmitButton } from '@brickninja-org/ui/components/form/buttons/SubmitButton';
 
 const getUserData = cache(async () => {
-  const session = await getUser();
-
-  if (!session) {
+  const userSession = await getUser();
+  if (!userSession) {
     redirect('/login');
   }
 
   const user = await db.user.findUnique({
-    where: { id: session.id },
+    where: { id: userSession.id },
     include: {
-      sessions: { orderBy: { lastUsed: 'desc' }},
+      sessions: { orderBy: { lastUsedAt: 'desc' }, where: { expiresAt: { gte: new Date() }}},
       providers: true,
     },
   });
@@ -29,13 +33,14 @@ const getUserData = cache(async () => {
   }
 
   return {
-    sessionId: session.sessionId,
+    sessionId: userSession.session.id,
     user,
   };
 });
 
 export default async function ProfilePage() {
-  const { user } = await getUserData();
+  const { sessionId, user } = await getUserData();
+  await pageView('profile');
 
   return (
     <HeroLayout hero={<Headline id="profile">{user.name}</Headline>} toc>
@@ -45,7 +50,7 @@ export default async function ProfilePage() {
 
       <Headline id="accounts">Accounts</Headline>
 
-      <Headline id="sessions">Sessions</Headline>
+      <Headline id="sessions" actions={<form action={revokeAllSessions}><SubmitButton icon="delete">Revoke all</SubmitButton></form>}>Sessions</Headline>
       <Table>
         <thead>
           <tr>
@@ -54,7 +59,41 @@ export default async function ProfilePage() {
             <th>Last Active</th>
           </tr>
         </thead>
+        <tbody>
+          {user.sessions.map((session) => (
+            <tr key={session.id}>
+              <td>{session.info}{session.id === sessionId && ' (Current Session)'}</td>
+              <td><FormatDate relative date={session.createdAt}/></td>
+              <td>{session.id === sessionId ? 'now' : <FormatDate relative date={session.lastUsedAt}/>}</td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </HeroLayout>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { user } = await getUserData();
+
+  return {
+    title: user.name,
+  };
+}
+
+async function revokeAllSessions() {
+  'use server';
+
+  const user = await getUser();
+
+  if (!user) {
+    return;
+  }
+
+  // delete all sessions except the current in db
+  await db.userSession.deleteMany({
+    where: { id: { not: user.session.id }, userId: user.id },
+  });
+
+  revalidatePath('/profile');
 }
