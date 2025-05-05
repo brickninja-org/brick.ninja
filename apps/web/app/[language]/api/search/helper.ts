a6335fc2df6c17325d011c4052cd3b4a586378fc
brickninja-pro <dev@brick.ninja>
2025-05-05 22:36:49 +0200

    chore(web): update search product id

--- apps/web/app/[language]/api/search/helper.ts
+++ apps/web/app/[language]/api/search/helper.ts
@@ -103,18 +103,20 @@ export const searchItems = cache(async (terms: string[], filter?: ItemFilters) =
 export const searchProducts = cache(async (terms: string[]) => {
   const nameQueries = nameQuery(terms);
 
- const [products, productCategories] = await Promise.all([
-  db.product.findMany({
-    where: terms.length > 0 ? { OR: nameQueries } : undefined,
-    take: 5,
-    include: { icon: true, categories: true },
-    orderBy: { views: 'desc' },
-  }),
-  db.category.findMany({
-    where: terms.length > 0 ? { OR: nameQueries } : undefined,
-    take: 5,
-  })
- ]);
+  const numberTerms = terms.map(toNumber).filter(isTruthy);
+
+  const [products, productCategories] = await Promise.all([
+    db.product.findMany({
+      where: terms.length > 0 ? { OR: nameQueries } : numberTerms.length > 0 ? { OR: [{ id: { in: numberTerms }}] } : undefined,
+      take: 5,
+      include: { icon: true, categories: true },
+      orderBy: { views: 'desc' },
+    }),
+    db.category.findMany({
+      where: terms.length > 0 ? { OR: nameQueries } : undefined,
+      take: 5,
+    })
+  ]);
 
  return { products, productCategories };
 }, ['search', 'search-products'], { revalidate: 60 });
