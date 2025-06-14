b11eac94195df5c27befdc44f14e191f82d0e181
brickninja-pro <dev@brick.ninja>
2025-06-14 23:20:48 +0200

    fix(web): update layout

--- apps/web/components/layout/Layout.tsx
+++ apps/web/components/layout/Layout.tsx
@@ -34,9 +34,9 @@ const Layout: FC<LayoutProps> = ({ children, language }) => {
     <div>
       <div className="layout grid min-h-dvh">
         <Menu navigation={<Navigation language={language}/>}>
-          <Link href="/" aria-label="brick.ninja" className="group relative flex items-center gap-4 mr-8 font-bitter font-bold text-xl text-foreground no-underline transition-transform">
+          <Link href="/" aria-label="brick-catlog.eu" className="group relative flex items-center gap-4 mr-8 font-bitter font-bold text-xl text-foreground no-underline transition-transform">
             <GiNinjaHead className="fill-red-800"/>
-            <span className="hidden group-hover:underline md:block">brick.ninja</span>
+            <span className="hidden group-hover:underline md:block">brick-catalog.eu</span>
           </Link>
           <Search translations={searchTranslations}/>
           <div className="flex -mr-2 ml-auto">
@@ -48,7 +48,7 @@ const Layout: FC<LayoutProps> = ({ children, language }) => {
         <hr className="[grid-area:menuShadow] sticky block top-12 h-[1px] bg-transparent z-2"/>
         {children}
         <footer className="[grid-area:footer] flex justify-between gap-4 flex-wrap py-8 px-4 border-t border-(--color-border)">
-          <span><b className="font-bitter font-semibold">brick.ninja</b> by @brickninja &copy; {new Date().getFullYear()}</span>
+          <span><b className="font-bitter font-semibold">brick-catalog.eu</b> by @brickninja &copy; {new Date().getFullYear()}</span>
           <div className="flex gap-4 flex-wrap">
             <Link href="/about">About</Link> /
             <Link href="/legal/legal-notice">Legal Notice</Link> /
