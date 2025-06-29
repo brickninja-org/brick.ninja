a2eeb679f65b04ba677865baf941f44665ad6f6e
brickninja-pro <dev@brick.ninja>
2025-06-29 22:59:25 +0200

    fix(web): update tooltip component

--- apps/web/components/tooltip/Tooltip.tsx
+++ apps/web/components/tooltip/Tooltip.tsx
@@ -45,6 +45,12 @@ export const Tooltip: FC<TooltipProps> = ({ children, content }) => {
 
   const { styles: transitionStyles, isMounted } = useTransitionStyles(context);
 
+  // @ts-expect-error this is a workaround, because react@19 somehow broke passing children elements (Tooltip is a client component, children is usually a server component)
+  //   before react@19 `children` was `<Lazy/>`, now it is `{ $$typeof: Symbol(react.lazy) }`.
+  //   We need access to the component props (especially ref) for the tooltip to function correctly
+  //   This seems to work for now, but I need to create a reproduction for this and report it to get it fixed.
+  if(children.$$typeof === Symbol.for('react.lazy')) { children = use(children._payload); }
+
   const ref = useMergeRefs(children.props.ref ? [refs.setReference, children.props.ref] : [refs.setReference]);
 
   return (
