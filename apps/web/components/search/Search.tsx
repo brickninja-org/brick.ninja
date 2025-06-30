115111b691b5a2556fdec393a9dccf2b633d8e4a
brickninja-pro <dev@brick.ninja>
2025-06-30 07:36:52 +0200

    fix(web): update search component add style

--- apps/web/components/search/Search.tsx
+++ apps/web/components/search/Search.tsx
@@ -155,7 +155,7 @@ export const Search: FC<SearchProps> = ({ translations }) => {
                     tabIndex={-1}
                     href={result.href}
                     key={result.href}
-                    className={cn(['grid gap-[0_8px] grid-cols-[32px_1fr_auto] py-2 px-4 rounded-2 text-foreground no-underline', activeIndex === currentIndex && 'bg-background-light'])}
+                    className={cn(['grid gap-x-2 gap-y-0 [grid-template-columns:32px_1fr_auto] py-2 px-4 rounded-2 text-foreground no-underline', activeIndex === currentIndex && 'bg-background-light'])}
                     id={result.href}
                     target={isExternal ? '_blank' : undefined}
                     rel={isExternal ? 'noreferrer noopener' : undefined}
@@ -163,6 +163,7 @@ export const Search: FC<SearchProps> = ({ translations }) => {
                     {...getItemProps({
                       onClick: (e) => !e.defaultPrevented && setOpen(false)
                     })}
+                    style={{ gridTemplateAreas: '"icon title external" "icon subtitle external"' }}
                   >
                     {result.icon}
                     <div className="[grid-area:title] line-clamp-1">
