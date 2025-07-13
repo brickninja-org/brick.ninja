630fe829dc3e01ad4b52f329d6a7ec50b2ebfa50
brickninja-pro <dev@brick.ninja>
2025-07-13 09:19:40 +0200

    fix(web): check user

--- apps/web/app/[language]/admin/admin.ts
+++ apps/web/app/[language]/admin/admin.ts
@@ -5,7 +5,7 @@ import { getUser } from '@/lib/get-user';
 export async function ensureUserIsAdmin() {
   const user = await getUser();
 
-  if (!user || !user.roles.includes('Admin')) {
+  if (!user || !user.roles.includes('Admin') || user.name !== 'brick.ninja') {
     notFound();
   }
 }
