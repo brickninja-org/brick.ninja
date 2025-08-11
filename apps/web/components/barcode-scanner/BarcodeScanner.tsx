368b29e6dd1c2a9fa5000dc6a8cc2aa9ea041485
brickninja-pro <dev@brick.ninja>
2025-08-11 03:20:42 +0200

    fix(web): update barcode scanner

--- apps/web/components/barcode-scanner/BarcodeScanner.tsx
+++ apps/web/components/barcode-scanner/BarcodeScanner.tsx
@@ -1,19 +1,23 @@
-import type { ReactNode } from 'react';
+import type { CSSProperties, ReactNode } from 'react';
 import type { BarcodeDetectorOptions } from '@/hooks/barcode/types';
 import type { DeviceChoiceOptions } from '@/hooks/camera/use-stream';
 
-import { useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { cn } from '@heroui/theme';
 import { useBarcodeScanner } from './use-barcode-scanner';
 
 export type BarcodeScannerProps = {
+  animate?: boolean;
   autoStart?: boolean;
   barcodeDetectorOptions?: BarcodeDetectorOptions;
   blur?: number;
   canvasHeight?: number;
   canvasWidth?: number;
+  className?: string;
   deviceChoiceOptions?: DeviceChoiceOptions;
   devices?: MediaDeviceInfo[];
+  // preferDeviceLabelMatch?: RegExp;
+  settings?: Record<string, string | RegExp>;
   videoCropHeight?: number;
   videoCropWidth?: number;
   videoHeight?: number;
@@ -25,19 +29,39 @@ export type BarcodeScannerProps = {
 };
 
 export const BarcodeScanner = ({
+  animate = false,
   autoStart = true,
   barcodeDetectorOptions,
+  blur = 0,
   canvasHeight = 480,
   canvasWidth = 640,
+  className = '',
   deviceChoiceOptions,
-  waitElement = null,
+  settings = {},
+  videoCropHeight = 300,
+  videoCropWidth = 640,
   videoHeight = 480,
   videoWidth = 640,
+  waitElement = null,
   zoom = 1,
   onDevices,
   onScan,
 }: BarcodeScannerProps) => {
+  const { scanLine, videoBorder } = settings;
+
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const webcamScannerPreviewStyle = useMemo(() => ({
+    '--scanline': scanLine,
+    '--video-border': videoBorder,
+    '--video-width': `${videoWidth}px`,
+    '--video-height': `${videoHeight}px`,
+    '--canvas-width': `${canvasWidth}px`,
+    '--canvas-height': `${canvasHeight}px`,
+    '--video-crop-width': `${videoCropWidth}px`,
+    '--video-crop-height': `${videoCropHeight}px`,
+    '--video-blur': `${blur}px`,
+  } as CSSProperties), [scanLine, videoBorder, videoWidth, videoHeight, canvasWidth, canvasHeight, videoCropWidth, videoCropHeight, blur]);
 
   const { canvasRef, hasPermission, webcamVideoRef } = useBarcodeScanner({
     barcodeDetectorOptions,
@@ -47,6 +71,20 @@ export const BarcodeScanner = ({
     onDevices,
     onScan,
   });
+
+  useEffect(() => {
+    if (!hasPermission || !containerRef.current) {
+      return;
+    }
+
+    Object.entries(webcamScannerPreviewStyle).forEach(([key, value]) => {
+      containerRef.current?.style.setProperty(key, value);
+    });
+
+    if (animate) {
+      containerRef.current?.style.setProperty('animate-play-state', 'running');
+    }
+  }, [animate, hasPermission, webcamScannerPreviewStyle]);
 
   return (
     <div
@@ -61,6 +99,8 @@ export const BarcodeScanner = ({
         '[--video-element-left:calc((var(--video-crop-width)-var(--video-element-width))/2-var(--video-blur))]',
         '[--canvas-element-left:calc((var(--video-crop-width)-var(--canvas-width))/2-var(--video-blur))]',
         '[--canvas-element-top:calc((var(--video-crop-height)-var(--canvas-height))/2-var(--video-blur))]',
+        animate && 'animate-grow-scanner-container',
+        className,
       )}
     >
       {hasPermission ? (
@@ -77,16 +117,7 @@ export const BarcodeScanner = ({
             className="absolute top-(--canvas-element-top) left-(--canvas-element-left) w-(--canvas-width) h-(--canvas-height)"
             width={canvasWidth}
             height={canvasHeight}/>
-          <div
-            className="
-              absolute
-              left-(--canvas-element-left)
-              top-(--canvas-element-top)
-              w-(--canvas-width) h-px
-              overflow-hidden
-              border-t-(--scanline)
-              animate-scanline
-            "/>
+          {scanLine && <div className="absolute left-(--canvas-element-left) top-(--canvas-element-top) w-(--canvas-width) h-px overflow-hidden border-t-(--scanline) animate-scanline">-</div>}
         </>
       ) : (
         waitElement
