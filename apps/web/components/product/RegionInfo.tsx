61f2eddb59e32f9790c1f5e3245b6401cd8aa4cd
brickninja-pro <dev@brick.ninja>
2025-08-23 23:00:37 +0200

    fix(web): update product region info

--- apps/web/components/product/RegionInfo.tsx
+++ apps/web/components/product/RegionInfo.tsx
@@ -10,35 +10,46 @@ import { fetchBrickNinjaApi } from '@brickninjaapi/fetch';
 import { useFormatContext } from '@/components/format/Format.context';
 import { DataList } from '@/components/info-box/DataList';
 import { FormatNumber } from '@/components/format/FormatNumber';
+import { FormatDate } from '../format/FormatDate';
 
 interface RegionInfoProps {
   data: Product;
   translations: TranslationSubset<
-    | 'priceGuide.rrp'
-    | 'priceGuide.ppp'
-    // | 'regionInfo.points'
+    | 'regionInfo.rrp'
+    | 'regionInfo.ppp'
+    | 'regionInfo.points'
+    | 'regionInfo.release.date'
+    | 'regionInfo.discontinuation.date'
   >;
 }
 
 export const RegionInfo: FC<RegionInfoProps> = ({ data, translations }) => {
   const { currency, region } = useFormatContext();
   const [regionData, setRegionData] = useState<RegionInfoData>();
-  // const [rrp, setRrp] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     fetchBrickNinjaApi(`/v1/products/${data.id}/region-info`, {}).then(setRegionData);
-    // setRrp(data.details?.region_info && (data.details.region_info[getApiRegion(region)]?.price / 100) || undefined);
   }, [data, region]);
 
-  const currentRegion = getApiRegion(region);
+  const currentRegion = regionData && regionData[getApiRegion(region)];
 
   return (
     <DataList data={[
-      regionData && regionData[currentRegion] ? { label: translations['priceGuide.rrp'], value: <FormatNumber value={regionData[currentRegion].price / 100} options={{ style: 'currency', currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'rrp' } : false,
-      regionData && regionData[currentRegion] && regionData[currentRegion].price >= 0 && data.details?.attributes ? { label: translations['priceGuide.ppp'], value: <FormatNumber value={regionData[currentRegion].price / (data.details?.attributes.find((attr) => attr.type === 'pieceCount')?.value as number)} options={{ style: 'currency', maximumFractionDigits: 3, currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'ppp' } : false,
-      // regionData && regionData[currentRegion] ? { label: translations['regionInfo.points'], value: <FormatNumber value={regionData[currentRegion].points}/>, key: 'points' } : false,
-      // rrp && rrp >= 0 ? { label: translations['priceGuide.rrp'], value: <FormatNumber value={rrp} options={{ style: 'currency', currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'rrp' } : false,
-      // rrp && rrp > 0 && data.details?.attributes ? { label: translations['priceGuide.ppp'], value: <FormatNumber value={rrp / (data.details.attributes.find((attr) => attr.type === 'pieceCount')?.value as number)} options={{ style: 'currency', maximumFractionDigits: 3, currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'ppp' } : false,
+      currentRegion && currentRegion.release_date
+        ? { label: translations['regionInfo.release.date'], value: <FormatDate date={currentRegion.release_date}/>, key: 'release_date' }
+        : false,
+      currentRegion && currentRegion.discontinuation_date
+        ? { label: translations['regionInfo.discontinuation.date'], value: <FormatDate date={currentRegion.discontinuation_date} relative/>, key: 'discontinuation_date' }
+        : false,
+      currentRegion
+        ? { label: translations['regionInfo.rrp'], value: <FormatNumber value={currentRegion.price / 100} options={{ style: 'currency', currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'rrp' }
+        : false,
+      currentRegion && currentRegion.price >= 0 && data.details?.attributes
+        ? { label: translations['regionInfo.ppp'], value: <FormatNumber value={(currentRegion.price / 100) / (data.details?.attributes.find((attr) => attr.type === 'pieceCount')?.value as number)} options={{ style: 'currency', maximumFractionDigits: 3, currency, currencyDisplay: 'narrowSymbol' }}/>, key: 'ppp' }
+        : false,
+      currentRegion && currentRegion.points
+        ? { label: translations['regionInfo.points'], value: <FormatNumber value={currentRegion.points}/>, key: 'points' }
+        : false,
     ]}/>
   );
 };
