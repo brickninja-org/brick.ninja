f94560084e110c51877087c014e54d57ae8ef1ea
brickninja-pro <dev@brick.ninja>
2025-08-29 16:58:55 +0200

    fix(web): improve admin job page

--- apps/web/app/[language]/admin/jobs/page.tsx
+++ apps/web/app/[language]/admin/jobs/page.tsx
@@ -1,32 +1,50 @@
 import type { Prisma } from '@brickninja-org/database';
 import type { FormState } from '@brickninja-org/ui/components/form/Form';
 
+import { Button, Input, Select, SelectItem } from '@heroui/react';
 import { Form } from '@brickninja-org/ui/components/form/Form';
 import { Headline } from '@brickninja-org/ui/components/headline/Headline';
-import { Label } from '@brickninja-org/ui/components/form/Label';
-import { TextInput } from '@brickninja-org/ui/components/form/TextInput';
 import { FlexRow } from '@brickninja-org/ui/components/flex-row/FlexRow';
-import { SubmitButton } from '@brickninja-org/ui/components/form/buttons/SubmitButton';
 
+import { cache } from '@/lib/cache';
 import { getUser } from '@/lib/get-user';
 import { createMetadata } from '@/lib/metadata';
 import { db } from '@/lib/prisma';
 import { PageLayout } from '@/components/layout/PageLayout';
 import { ensureUserIsAdmin } from '../admin';
 
+const getJobsTypes = cache(async () => {
+  const jobs = await db.job.findMany({ select: { type: true }, distinct: ['type'], orderBy: { type: 'asc' }});
+  return jobs.map((j) => j.type);
+}, ['get-job-types'], { revalidate: 60 });
+
 export default async function AdminQueueJobPage() {
   await ensureUserIsAdmin();
+
+  const jobs = await getJobsTypes();
 
   return (
     <PageLayout>
       <Headline id="apps">Queue Job</Headline>
       <Form action={submit}>
-        <Label label="Type"><TextInput name="type"/></Label>
+        <Select
+          className="max-w-xs"
+          items={jobs.map((job) => ({ value: job, label: job }))}
+          label="Type"
+          name="type"
+          placeholder="Select a job type"
+        >
+          {(job) => <SelectItem>{job.label}</SelectItem>}
+        </Select>
 
-        <Label label="Data"><TextInput name="data" defaultValue="{}"/></Label>
+        <Input
+          className="max-w-xs"
+          label="Data"
+          name="data"
+          defaultValue="{}"/>
 
         <FlexRow>
-          <SubmitButton>Queue</SubmitButton>
+          <Button type="submit">Queue</Button>
         </FlexRow>
       </Form>
     </PageLayout>
