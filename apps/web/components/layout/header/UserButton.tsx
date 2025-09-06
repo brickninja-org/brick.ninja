de2361aadefa04878007648358dfdd2ffb46870c
brickninja-pro <dev@brick.ninja>
2025-09-06 20:20:13 +0200

    fix(web): improve user button

--- apps/web/components/layout/header/UserButton.tsx
+++ apps/web/components/layout/header/UserButton.tsx
@@ -3,20 +3,15 @@ import type { Language } from '@brickninja-org/database';
 import type { SessionUser } from '@/lib/get-user';
 
 import { Suspense } from 'react';
-import { Button, Link } from '@heroui/react';
-
-import { Dropdown } from '@brickninja-org/ui/components/dropdown/Dropdown';
-import { LinkButton } from '@brickninja-org/ui/components/form/Button';
-import { SubmitButton } from '@brickninja-org/ui/components/form/buttons/SubmitButton';
-import { MenuList } from '@brickninja-org/ui/components/layout/MenuList';
-import { Separator } from '@brickninja-org/ui/components/layout/Separator';
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, Skeleton } from '@heroui/react';
+import { UserRole } from '@brickninja-org/database';
 import { Icon } from '@brickninja-org/ui/icons';
 
 import { getUser } from '@/lib/get-user';
 import { getTranslate } from '@/lib/translate';
-import { reauthorize } from '@/components/bn2-api/reauthorize';
-import { Skeleton } from '@/components/skeleton/Skeleton';
 import { Translate } from '@/components/i18n/Translate';
+import { reauthorize } from '@/components/bn2-api/reauthorize';
+import { SubmitButton } from '@/components/form/SubmitButton';
 
 export interface UserButtonProps {
   language: Language;
@@ -24,16 +19,11 @@ export interface UserButtonProps {
 
 export const UserButton: FC<UserButtonProps> = ({ language }) => {
   return (
-    <Suspense fallback={<UserButtonInternal language={language} user="loading"/>}>
+    <Suspense>
       <UserButtonLoader language={language}/>
     </Suspense>
   );
 };
-
-interface UserButtonInternalProps {
-  user?: SessionUser | 'loading';
-  language: Language;
-}
 
 // internal component to load the user
 const UserButtonLoader: FC<UserButtonProps> = async ({ language }) => {
@@ -42,39 +32,94 @@ const UserButtonLoader: FC<UserButtonProps> = async ({ language }) => {
   return <UserButtonInternal language={language} user={user}/>;
 };
 
+interface UserButtonInternalProps {
+  user?: SessionUser | 'loading';
+  language: Language;
+}
+
 // internal component to show loader | user | login
 const UserButtonInternal: FC<UserButtonInternalProps> = ({ user, language }) => {
   const t = getTranslate(language);
 
   if (!user) {
     return (
-      <Button as={Link} radius="sm" variant="light" href="/login" className="min-w-10 w-10 md:min-w-20 md:w-fit" aria-label={t('login')}>
-        <Icon icon="user"/><span className="hidden md:block"> <Translate id="login" language={language}/></span>
+      <Button
+        aria-label={t('login')}
+        className="min-w-10 w-10 md:min-w-20 md:w-fit"
+        href="/login"
+        radius="sm"
+        startContent={<Icon icon="user"/>}
+        variant="light"
+      >
+        <span className="hidden md:block">
+          <Translate id="login" language={language}/>
+        </span>
       </Button>
     );
   }
 
   const button = (
-    <Button as={Link} startContent={<Icon icon="user"/>} radius="sm" variant="light" href="/profile" className="min-w-10 w-10 md:min-w-20 md:w-fit" aria-label={user === 'loading' ? undefined : user.name}>
-      <span className="hidden md:block">{user === 'loading' ? <Skeleton width={90}/> : user.name}</span>
+    <Button
+      aria-label={user === 'loading' ? undefined : user.name}
+      className="min-w-10 w-10 md:min-w-20 md:w-fit"
+      href="/profile"
+      radius="sm"
+      startContent={<Icon icon="user"/>}
+      variant="light"
+    >
+      <span className="hidden md:block">{user === 'loading' ? <Skeleton className="w-20"/> : user.name}</span>
     </Button>
   );
 
   return (
-    <Dropdown hideTop={false} button={button} preferredPlacement="bottom">
-      <MenuList>
-        <LinkButton appearance="menu" href="/profile" icon="user">Profile</LinkButton>
-        {user !== 'loading' && user.roles.includes('Admin') && (
-          <LinkButton appearance="menu" icon="developer" href="/admin/users">Admin</LinkButton>
-        )}
-        <form action="/logout" method="POST" className="flex">
-          <SubmitButton appearance="menu" icon="logout" flex>Logout</SubmitButton>
-        </form>
-        <Separator/>
-        <form action={reauthorize.bind(null, [], 'consent')} className="flex">
-          <SubmitButton appearance="menu" icon="unlock" flex>Manage Accounts</SubmitButton>
-        </form>
-      </MenuList>
+    <Dropdown offset={8} placement="bottom-end" radius="sm" shadow="md">
+      <DropdownTrigger>{button}</DropdownTrigger>
+      <DropdownMenu>
+        <DropdownSection showDivider title="actions">
+          <DropdownItem
+            key="profile"
+            href="/profile"
+            startContent={<Icon icon="user"/>}
+          >
+            <Translate id="user.profile" language={language}/>
+          </DropdownItem>
+          {user !== 'loading' && user.roles.includes(UserRole.Admin) ? (
+            <DropdownItem
+              key="admin"
+              href="/admin/users"
+              startContent={<Icon icon="developer"/>}
+            >
+              <Translate id="user.role.admin" language={language}/>
+            </DropdownItem>
+          ) : null}
+          <DropdownItem key="logout">
+            <form action="/logout" method="POST" className="flex w-full">
+              <SubmitButton
+                radius="sm"
+                size="sm"
+                startContent={<Icon icon="logout"/>}
+                variant="light"
+              >
+                <Translate id="logout" language={language}/>
+              </SubmitButton>
+            </form>
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownSection title="accounts">
+          <DropdownItem key="manage-accounts">
+            <form action={reauthorize.bind(null, [], 'consent')} className="flex w-full">
+              <SubmitButton
+                radius="sm"
+                size="sm"
+                startContent={<Icon icon="unlock"/>}
+                variant="light"
+              >
+                <Translate id="user.manage-accounts" language={language}/>
+              </SubmitButton>
+            </form>
+          </DropdownItem>
+        </DropdownSection>
+      </DropdownMenu>
     </Dropdown>
   );
 };
