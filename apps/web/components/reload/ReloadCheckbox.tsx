88ee92148d806222634605c5a203c184d634aa49
brickninja-pro <dev@brick.ninja>
2025-09-07 20:15:31 +0200

    fix(web): update reload checkbox

--- apps/web/components/reload/ReloadCheckbox.tsx
+++ apps/web/components/reload/ReloadCheckbox.tsx
@@ -5,7 +5,7 @@ import type { RefProp } from '@brickninja-org/ui/lib/react';
 import type { ReloadProps } from './Reload';
 
 import { useState } from 'react';
-import { Checkbox } from '@heroui/react';
+import { Checkbox, cn } from '@heroui/react';
 
 import { Reload } from './Reload';
 
@@ -15,7 +15,20 @@ export const ReloadCheckbox: FC<ReloadProps & RefProp<HTMLInputElement>> = (({ r
   return (
     <>
       {autoRefresh && <Reload {...reloadProps}/>}
-      <Checkbox isSelected={autoRefresh} radius="sm" color="primary" onValueChange={setAutorefresh} ref={ref}>
+      <Checkbox
+        ref={ref}
+        aria-label="Auto Refresh"
+        isSelected={autoRefresh}
+        classNames={{
+          base: cn(
+            'inline-flex items-center justify-start gap-2',
+            'rounded-sm bg-content1 hover:bg-content2'
+          ),
+        }}
+        color="primary"
+        radius="sm"
+        onValueChange={setAutorefresh}
+      >
         Auto Refresh
       </Checkbox>
     </>
