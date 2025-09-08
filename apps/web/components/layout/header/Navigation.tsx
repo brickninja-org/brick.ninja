c4ada072e7533c0fdf0dfff69e3d2844547570e3
brickninja-pro <dev@brick.ninja>
2025-09-08 23:00:38 +0200

    fix(web): remove active state handling from NavigationItem components

--- apps/web/components/layout/header/Navigation.tsx
+++ apps/web/components/layout/header/Navigation.tsx
@@ -15,8 +15,8 @@ const Navigation: FC<NavigationProps> = ({ language }) => {
   return (
     <HorizontalOverflowContainer>
       <Composite render={<ul className="flex py-1.5 bg-background first:border-l-0"/>}>
-        <NavigationItem href="/item" active={pathname.startsWith('/item')}><Translate language={language} id="navigation.items"/></NavigationItem>
-        <NavigationItem href="/sets" active={pathname.startsWith('/sets')}><Translate language={language} id="navigation.products"/></NavigationItem>
+        <NavigationItem href="/item"><Translate language={language} id="navigation.items"/></NavigationItem>
+        <NavigationItem href="/sets"><Translate language={language} id="navigation.products"/></NavigationItem>
       </Composite>
     </HorizontalOverflowContainer>
   );
