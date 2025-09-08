6968d08c475e14f24b6ed4eabbbd7716a53fb822
brickninja-pro <dev@brick.ninja>
2025-09-08 20:01:25 +0200

    fix(web): enhance metadata generation with language context and improve type definitions

--- apps/web/lib/metadata.ts
+++ apps/web/lib/metadata.ts
@@ -1,16 +1,37 @@
-import type { Metadata } from 'next';
-import type { LayoutProps, PageProps } from './next';
-import { getAlternateUrls, getCurrentUrl } from './url';
+import type { Metadata, ResolvingMetadata } from 'next';
 import type { TemplateString } from 'next/dist/lib/metadata/types/metadata-types';
+import type { Language } from '@brickninja-org/database';
+import type { LayoutProps, PageProps } from './next';
+
+import { getAlternateUrls, getCurrentUrl } from './url';
 import { getLanguage } from './translate';
 
-export function createMetadata<Props extends PageProps | LayoutProps>(getMeta: ((props: Props) => Promise<Meta> | Meta) | Meta) {
-  return async (props: Props): Promise<Metadata> => {
-    const meta = typeof getMeta === 'function' ? await getMeta(props) : getMeta;
+interface CreateMetadataContext {
+  language: Language;
+}
+
+type CreateMetadataCallback<T> = (props: T, context: CreateMetadataContext) => Promise<Meta> | Meta;
+
+type GenerateMetadata<T> = (props: T, parent: ResolvingMetadata) => Promise<Metadata>;
+
+export function createMetadata<Props extends PageProps | LayoutProps>(
+  getMeta: CreateMetadataCallback<Props> | Meta
+): GenerateMetadata<Props> {
+  return async (props) => {
     const language = await getLanguage();
 
+    // get meta from callback or object
+    const meta = typeof getMeta === 'function'
+      ? await getMeta(props, { language })
+      : getMeta;
+
+    // generate alternate urls
+    // TODO: require `meta.url` to be set for all pages to remove fallback to dynamic `getCurrentUrl`
     const url = await getCurrentUrl();
     const alternates = getAlternateUrls(meta.url ?? url.pathname, language);
+
+    // get metadata from parent (e.g. layout)
+    // const parentMeta = await parent;
 
     return {
       title: meta.title,
@@ -24,6 +45,7 @@ export function createMetadata<Props extends PageProps | LayoutProps>(getMeta: (
 
 type Meta = {
   title: string | TemplateString;
+  ogTitle?: string;
   description?: string;
   keywords?: string[];
   url?: string;
