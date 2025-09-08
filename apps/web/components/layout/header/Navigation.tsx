e8fa2d3311b69114ff0497274257bcbebf8f1576
brickninja-pro <dev@brick.ninja>
2025-09-08 22:46:47 +0200

    fix(web): update navigation items to reflect active state based on pathname

--- apps/web/components/layout/header/Navigation.tsx
+++ apps/web/components/layout/header/Navigation.tsx
@@ -1,22 +1,27 @@
+'use client';
+
 import type { FC, ReactNode } from 'react';
 import type { Language } from '@brickninja-org/database';
 
-import { LinkButton } from '@brickninja-org/ui/components/form/Button';
+import { Button, Link } from '@heroui/react';
 import { Composite, CompositeItem } from '@brickninja-org/ui/components/focus/Composite';
 
 import { Translate } from '@/components/i18n/Translate';
 import { HorizontalOverflowContainer } from '@/components/layout/HorizontalOverflowContainer';
+import { usePathname } from 'next/navigation';
 
 interface NavigationProps {
   language: Language;
 }
 
 const Navigation: FC<NavigationProps> = ({ language }) => {
+  const pathname = usePathname();
+
   return (
     <HorizontalOverflowContainer>
       <Composite render={<ul className="flex py-1.5 bg-background first:border-l-0"/>}>
-        <NavigationItem href="/item"><Translate language={language} id="navigation.items"/></NavigationItem>
-        <NavigationItem href="/sets"><Translate language={language} id="navigation.products"/></NavigationItem>
+        <NavigationItem href="/item" active={pathname.startsWith('/item')}><Translate language={language} id="navigation.items"/></NavigationItem>
+        <NavigationItem href="/sets" active={pathname.startsWith('/sets')}><Translate language={language} id="navigation.products"/></NavigationItem>
       </Composite>
     </HorizontalOverflowContainer>
   );
@@ -25,12 +30,21 @@ const Navigation: FC<NavigationProps> = ({ language }) => {
 interface NavigationItemProps {
   children: ReactNode;
   href: string;
+  active?: boolean;
 }
 
-export const NavigationItem: FC<NavigationItemProps> = (props) => {
+export const NavigationItem: FC<NavigationItemProps> = ({ active = false, ...buttonProps }) => {
   return (
-    <li className="border-l border-(--color-border)">
-      <CompositeItem render={<LinkButton appearance="menu" {...props}/>}/>
+    <li className="border-l border-default">
+      <CompositeItem
+        render={(
+          <Button
+            as={Link}
+            color={active ? 'primary' : 'default'}
+            radius="sm"
+            variant={active ? 'solid' : 'light'}
+            {...buttonProps}/>
+        )}/>
     </li>
   );
 };
