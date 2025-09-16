17a91bfa6506b4f0d0f4b94e8d59b568e1f46c65
brickninja-pro <dev@brick.ninja>
2025-09-16 23:33:59 +0200

    feat(spinner): add Spinner component with styles and variants

---
+++ apps/web/components/spinner/index.ts
@@ -0,0 +1,4 @@
+export { Spinner } from './spinner.client';
+export type { SpinnerProps } from './spinner.client';
+export { spinnerVariants } from './spinner.styles';
+export type { SpinnerVariants } from './spinner.styles';
