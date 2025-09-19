0cc335015da2c9a79e9074e3185da8805e64a099
brickninja-pro <dev@brick.ninja>
2025-09-19 15:00:29 +0200

    refactor(button): update internal Button component import and type alias

--- apps/web/components/button/button.tsx
+++ apps/web/components/button/button.tsx
@@ -1,22 +1,26 @@
 import type { FC } from 'react';
 import type { IconName } from '@/components/iconify';
-import type { ButtonProps as ButtonInternalProps } from './button.client';
+import type { ButtonProps as HeroUIButtonProps } from './button.client';
 
 import { Iconify } from '@/components/iconify';
-import { Button as ButtonInternal } from './button.client';
+import { Button as HeroUIButton } from './button.client';
 import { cn } from '@heroui/react';
 
-interface ButtonProps extends ButtonInternalProps {
+interface ButtonProps extends HeroUIButtonProps {
   flex?: boolean,
   icon?: IconName,
 }
 
 const Button: FC<ButtonProps> = ({ children, className, flex, icon, ...props }) => {
   return (
-    <ButtonInternal className={cn({ 'flex-1': flex, className })} {...props}>
-      {icon && <Iconify icon={icon}/>}
-      {typeof children === 'function' ? children({} as never) : children}
-    </ButtonInternal>
+    <HeroUIButton className={cn({ 'flex-1': flex, className })} {...props}>
+      {(renderProps) => (
+        <>
+          {icon && <Iconify icon={icon}/>}
+          {typeof children === 'function' ? children(renderProps) : children}
+        </>
+      )}
+    </HeroUIButton>
   );
 };
 
