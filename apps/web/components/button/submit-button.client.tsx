300706811fe081224c7afc49623571e579b52264
brickninja-pro <dev@brick.ninja>
2025-09-20 01:30:49 +0200

    refactor(web): optimize SubmitButton component by adjusting imports and spinner size

--- apps/web/components/button/submit-button.client.tsx
+++ apps/web/components/button/submit-button.client.tsx
@@ -4,9 +4,8 @@ import type { FC, ReactNode } from 'react';
 import type { ButtonProps } from '@/components/button';
 
 import { useFormStatus } from 'react-dom';
-import { Spinner } from '@heroui/react';
+import { Button, Spinner } from '@heroui/react';
 
-import { Button } from '@/components/button';
 import { Iconify } from '@/components/iconify';
 
 interface SubmitButtonProps extends Omit<ButtonProps, 'asChild' | 'children' | 'isPending' | 'type'> {
@@ -20,15 +19,13 @@ const SubmitButton: FC<SubmitButtonProps> = ({ icon, children, ...props }) => {
     <Button type="submit" isDisabled={pending} isPending={pending} {...props}>
       {({ isPending }) => (
         <>
-          {isPending ? <Spinner/> : icon && <Iconify icon={icon}/>}
+          {isPending ? <Spinner size="sm"/> : icon && <Iconify icon={icon}/>}
           {isPending ? 'Submitting...' : children}
         </>
       )}
     </Button>
   );
 };
-
-SubmitButton.displayName = 'BrickCatalog.SubmitButton';
 
 export type { SubmitButtonProps };
 export { SubmitButton };
