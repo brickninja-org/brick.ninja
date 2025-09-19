f463004c73455a511401a6efd1f73039d74d089d
brickninja-pro <dev@brick.ninja>
2025-09-19 19:51:21 +0200

    refactor(QueueJobForm): streamline form submission and improve admin checks

--- apps/web/app/[language]/admin/jobs/QueueJobForm.tsx
+++ apps/web/app/[language]/admin/jobs/QueueJobForm.tsx
@@ -1,54 +1,74 @@
-'use client';
+import type { Prisma } from '@brickninja-org/database';
+import type { FormState } from '@brickninja-org/ui/components/form/Form';
 
-import { cn, Input, Select, SelectItem, type InputProps, type SelectProps } from '@heroui/react';
+import { Form } from '@brickninja-org/ui/components/form/Form';
+import { Headline } from '@brickninja-org/ui/components/headline/Headline';
+import { Label } from '@brickninja-org/ui/components/form/Label';
+import { TextInput } from '@brickninja-org/ui/components/form/TextInput';
 import { FlexRow } from '@brickninja-org/ui/components/flex-row/FlexRow';
+import { SubmitButton } from '@/components/button';
 
-import { Form } from '@/components/form/Form';
-import { SubmitButton } from '@/components/form/SubmitButton';
-import { submit } from './actions';
+import { getUser } from '@/lib/get-user';
+import { createMetadata } from '@/lib/metadata';
+import { db } from '@/lib/prisma';
+import { PageLayout } from '@/components/layout/PageLayout';
+import { ensureUserIsAdmin } from '../admin';
 
-export interface QueueJobFormProps {
-  jobs: { key: string, label: string }[],
-  className?: string,
-}
+export default async function AdminQueueJobPage() {
+  await ensureUserIsAdmin();
 
-export function QueueJobForm({ className, jobs }: QueueJobFormProps) {
-  const inputProps: Pick<InputProps, 'labelPlacement' | 'classNames'> = {
-    labelPlacement: 'outside',
-    classNames: {
-      label: 'text-small font-medium text-default-700 group-data-[filled-within=true]:text-default-700',
-    }
-  };
-
-  const selectProps: Pick<SelectProps, 'labelPlacement' | 'classNames'> = {
-    labelPlacement: 'outside',
-    classNames: {
-      label: 'text-small font-medium text-default-700 group-data-[filled=true]:text-default-700',
-    }
-  };
   return (
-    <Form id="queue" className={cn(className)} action={submit}>
-      <Select
-        className="max-w-xs"
-        items={jobs}
-        label="Type"
-        name="type"
-        placeholder="Select a job type"
-        {...selectProps}
-      >
-        {(job) => <SelectItem>{job.label}</SelectItem>}
-      </Select>
+    <PageLayout>
+      <Headline id="apps">Queue Job</Headline>
+      <Form action={submit}>
+        <Label label="Type"><TextInput name="type"/></Label>
 
-      <Input
-        className="max-w-xs"
-        label="Data"
-        name="data"
-        defaultValue="{}"
-        {...inputProps}/>
+        <Label label="Data"><TextInput name="data" defaultValue="{}"/></Label>
 
-      <FlexRow>
-        <SubmitButton variant="ghost">Queue</SubmitButton>
-      </FlexRow>
-    </Form>
+        <FlexRow>
+          <SubmitButton>Queue</SubmitButton>
+        </FlexRow>
+      </Form>
+    </PageLayout>
   );
 }
+
+async function submit(_: FormState, payload: FormData): Promise<FormState> {
+  'use server';
+
+  const user = await getUser();
+  if(!user || !user.roles.includes('Admin')) {
+    return { error: 'Not authorized' };
+  }
+
+  const type = payload.get('type');
+  const rawData = payload.get('data');
+
+  if(typeof type !== 'string') {
+    return { error: 'Invalid type' };
+  }
+  if(typeof rawData !== 'string') {
+    return { error: 'Invalid data' };
+  }
+
+  let data: Prisma.JsonObject;
+
+  try {
+    data = JSON.parse(rawData);
+  } catch {
+    return { error: 'Invalid data' };
+  }
+
+  await db.job.create({
+    data: {
+      type,
+      data,
+    }
+  });
+
+  return { success: 'Queued' };
+}
+
+export const generateMetadata = createMetadata({
+  title: 'Jobs',
+});
