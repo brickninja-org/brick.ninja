367bb8af86e05b79ca735540888c2df9e3a613eb
brickninja-pro <dev@brick.ninja>
2025-09-20 18:29:52 +0200

    refactor(web): update Dialog component styling for consistency and improved appearance

--- packages/ui/components/dialog/Dialog.tsx
+++ packages/ui/components/dialog/Dialog.tsx
@@ -36,10 +36,10 @@ export const Dialog: FC<DialogProps> = ({ children, title, open, initialFocus, o
     <FloatingPortal>
       <FloatingOverlay className="fixed top-0 bottom-0 left-0 right-0 flex justify-center bg-black/5 backdrop-blur-[1px] z-10" style={transitionStyles}>
         <FloatingFocusManager context={context} initialFocus={initialFocus}>
-          <div ref={refs.setFloating} aria-labelledby={labelId} className="absolute top-14 sm:top-32 flex flex-col mx-auto max-w-none sm:max-w-[calc(100vw-100px)] max-h-[calc(100vh-72px)] sm:max-h-[calc(100vh-160px)] min-w-0 sm:min-w-[500px] w-[calc(100vw-32px)] sm:w-auto rounded-xs shadow border bg-background overflow-hidden [--table-sticky-top:-16px]" {...getFloatingProps()}>
+          <div ref={refs.setFloating} aria-labelledby={labelId} className="absolute top-14 sm:top-32 flex flex-col mx-auto max-w-none sm:max-w-[calc(100vw-100px)] max-h-[calc(100vh-72px)] sm:max-h-[calc(100vh-160px)] min-w-0 sm:min-w-[500px] w-[calc(100vw-32px)] sm:w-auto rounded-md shadow-md border bg-panel overflow-hidden [--table-sticky-top:-16px]" {...getFloatingProps()}>
             <div className="flex items-center p-2 pr-4 border-b leading-6 font-medium">
               <header id={labelId}>{title}</header>
-              <button type="button" className="flex items-center justify-center w-8 h-8 ml-auto p-2 rounded-xs bg-background hover:bg-background-light text-muted leading-5 cursor-pointer will-change-transform transition-background" onClick={onClose}><Icon icon="close"/></button>
+              <button type="button" className="flex items-center justify-center w-8 h-8 ml-auto p-2 rounded-xs bg-panel hover:bg-panel-hover text-muted leading-5 cursor-pointer will-change-transform transition-background" onClick={onClose}><Icon icon="close"/></button>
             </div>
             <div className="p-4 overflow-auto overscroll-contain">
               <TableOfContentContext>
