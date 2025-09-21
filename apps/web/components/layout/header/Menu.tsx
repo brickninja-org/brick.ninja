529dd6a30f8287286b7044a480746f579ea6b5c0
brickninja-pro <dev@brick.ninja>
2025-09-21 16:05:05 +0200

    fix(web): update Menu component

--- apps/web/components/layout/header/Menu.tsx
+++ apps/web/components/layout/header/Menu.tsx
@@ -35,7 +35,7 @@ export const Menu: FC<MenuProps> = ({ children, navigation }) => {
     <div className="[grid-area:_menu]">
       <header
         className={cn([
-          'fixed top-0 inset-x-0',
+          'fixed top-0 left-0 right-0',
           'flex items-center gap-4 md:gap-8',
           'h-12 px-4',
           'bg-background',
@@ -49,10 +49,10 @@ export const Menu: FC<MenuProps> = ({ children, navigation }) => {
             'absolute',
             'h-12 w-12',
             '-mx-4',
-            'bg-transparent',
-            'opacity-0 [transition:_opacity_.1s_ease] delay-0',
+            'bg-transparent text-foreground',
+            'opacity-0 transition-opacity delay-0',
             'cursor-pointer pointer-events-none',
-            scrolledDown && 'opacity-100 delay-150 pointer-events-auto',
+            scrolledDown && 'opacity-100 delay-150 [pointer-events:all]',
           ])}
           onClick={() => setMenuOpen(!menuOpen)}
           tabIndex={-1}
@@ -66,7 +66,7 @@ export const Menu: FC<MenuProps> = ({ children, navigation }) => {
       <nav
         className={cn([
           'absolute left-0 right-0 mt-12 z-9',
-          menuOpen && 'fixed border-b border-transparent animate-menu-slide-in',
+          menuOpen && 'fixed border-b border-transparent animate-nav-slide-in',
         ])}
       >
         {navigation}
