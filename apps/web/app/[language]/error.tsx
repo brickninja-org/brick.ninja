9aa7ad8b56358fc896e04f494fa7278f5c8b75b5
brick.ninja <dev@brick.ninja>
2025-09-27 00:53:11 +0200

    fix(web): update error component to use Tailwind CSS classes for styling

--- apps/web/app/[language]/error.tsx
+++ apps/web/app/[language]/error.tsx
@@ -7,7 +7,7 @@ import { HeroLayout } from '@/components/layout/HeroLayout';
 export default function Error({ error }: { error: Error }) {
   return (
     <HeroLayout hero={<Headline id="error">Something went wrong!</Headline>}>
-      <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
+      <pre className="whitespace-pre-wrap break-all">
         {error.stack && (process.env.NODE_ENV === 'production' ? window.btoa(error.stack) : error.stack)}
       </pre>
     </HeroLayout>
